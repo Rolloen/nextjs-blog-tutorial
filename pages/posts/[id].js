c6e26b108cc39b333b9f8b8c660648c1f597ec23
Qixin YING <ying.qixin@gmail.com>
2023-10-24 10:35:45 +0200

    final step tutorial

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
@@ -15,20 +18,33 @@ export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
 export default function Post({postData}) {
+    const router = useRouter()
+ 
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+ 
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br/>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXl}>
+                {postData.title}
+            </h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     </Layout>
   )
 }
